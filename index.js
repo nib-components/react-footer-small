a8ecde580465b512a0ae18c9a90e9e57b2eb946e
Laurie Jones <laurie.jones@nib.com.au>
2016-02-10 11:41:30 +1100

    Add fund rules link to footer

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ export default class FooterSmall extends React.Component {
             <div className="nib-footer__copyright-text">Copyright <span>&copy;</span> {currentYear} nib health funds limited ABN 83 000 124 381</div>
 
             <nav id="ga-footer-copyright-links" className="nib-footer__copyright-links">
+              <a className="nib-footer__copyright-link" id="ga-footer-fund-rules" href="https://www.nib.com.au/health-information/content/assets/pdf/nib-fund-rules.pdf">Fund Rules</a>
               <a className="nib-footer__copyright-link" id="ga-footer-terms" href="https://www.nib.com.au/legal/terms-and-conditions">Terms &amp; Conditions</a>
               <a className="nib-footer__copyright-link" id="ga-footer-privacy" href="https://www.nib.com.au/legal#privacy">Privacy Policy</a>
               <a className="nib-footer__copyright-link" id="ga-footer-code-conduct" href="https://www.nib.com.au/legal#codeofconduct">Code of Conduct</a>
