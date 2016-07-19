5d5a95a6b2669ff4ae10a8710326933f6fc1e957
James Newell <j.newell@nib.com.au>
2016-07-19 14:59:02 +1000

    fix deps and clean up files which should be ignored

--- index.js
+++ index.js
@@ -6,18 +6,25 @@ export default class FooterSmall extends React.Component {
     return (
       <footer className="nib-footer nib-footer--small">
         <div className="nib-footer__body">
+
           <div className="nib-footer__copyright">
-            <div className="nib-footer__copyright-text">Copyright <span>&copy;</span> {currentYear} nib health funds limited ABN 83 000 124 381</div>
+
+            <div className="nib-footer__copyright-text">
+              Copyright <span>&copy;</span> {currentYear} nib health funds limited ABN 83 000 124 381
+            </div>
 
             <nav id="ga-footer-copyright-links" className="nib-footer__copyright-links">
               <a className="nib-footer__copyright-link" id="ga-footer-fund-rules" href="https://www.nib.com.au/health-information/content/assets/pdf/nib-fund-rules.pdf">Fund Rules</a>
               <a className="nib-footer__copyright-link" id="ga-footer-terms" href="https://www.nib.com.au/legal/terms-and-conditions">Terms &amp; Conditions</a>
               <a className="nib-footer__copyright-link" id="ga-footer-privacy" href="https://www.nib.com.au/legal#privacy">Privacy Policy</a>
               <a className="nib-footer__copyright-link" id="ga-footer-code-conduct" href="https://www.nib.com.au/legal#codeofconduct">Code of Conduct</a>
-              <a className="nib-footer__copyright-link nib-footer__copyright-link--phone-number" id="ga-footer-call-mobile" href="tel:131642"><i class="v2-icon v2-icon--phone"/>13 16 42</a>
+              <a className="nib-footer__copyright-link nib-footer__copyright-link--phone-number" id="ga-footer-call-mobile" href="tel:131642">
+                <i class="v2-icon v2-icon--phone"/>13 16 42
+              </a>
             </nav>
         </div>
       </div>
+
     </footer>
   );
   }
